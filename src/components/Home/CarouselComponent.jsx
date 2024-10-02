160afd400e2ce5af1002732eef31dd463ac1d669
kunal112299 <kunal11229900@gmail.com>
2024-10-02 18:22:25 +0530

    updated code-carousal

--- src/components/Home/CarouselComponent.jsx
+++ src/components/Home/CarouselComponent.jsx
@@ -5,11 +5,11 @@ function CarouselComponent() {
   return (
     <div className="h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]">
       <Carousel pauseOnHover>
-        <img src="img1.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[50vh]'/>
-        <img src="img2.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[50vh]'/>
-        <img src="img3.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[50vh]'/>
-        <img src="img4.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[50vh]'/>
-        <img src="img5.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[50vh]'/>
+        <img src="img1.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]'/>
+        <img src="img2.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]'/>
+        <img src="img3.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]'/>
+        <img src="img4.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]'/>
+        <img src="img5.jpg" alt="..." className='h-[50vh] sm:h-[60vh] xl:h-[85vh] 2xl:h-[85vh]'/>
       </Carousel>
     </div>
   );
