b579e7ba385d8192eba84bd0af171b3fbcc6b781
kunal112299 <kunal11229900@gmail.com>
2024-10-04 00:10:44 +0530

    fdg

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import MainProject from './components/projects/main.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import ProjectList from './components/projects/projectList.jsx'
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>
-  },
-  {
-    path: '/project',
-    element: <MainProject/>
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/project" element={<MainProject/>}/>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
