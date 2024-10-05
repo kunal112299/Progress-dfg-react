7a217788e0015a53fe69a6e33b3ebfd00bc24ca8
Kunal Singh <76222845+kunal112299@users.noreply.github.com>
2024-10-05 11:23:47 +0530

    Update AboutUsDesc.jsx

--- src/components/AboutUs/AboutUsDesc.jsx
+++ src/components/AboutUs/AboutUsDesc.jsx
@@ -4,11 +4,11 @@ function AboutUsDesc() {
   return (
     <div>
       <div className="w-full mt-24 px-5">
-      <h1 className="flex justify-center text-xl md:text-3xl text-[#333] karla-regular">About us</h1>
+      <h1 className="flex justify-center text-xl md:text-4xl text-[#333] karla-regular">About us</h1>
       <hr className="w-20 h-[1px] mx-auto my-8 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
     </div>
       <div>
-        <p className="text-center px-20 lg:px-48 md:text-[16px] font-bold text-[#003c56] text-[10px] italic">
+        <p className="text-center px-20 lg:px-48 md:text-[24px] font-bold text-[#003c56] text-[10px] italic">
           We turn your dream spaces into reality with expert design and construction services,<br/> encompassing everything from initial planning to the final build. 
           <br/> Our dedicated team ensures a seamless and personalized experience for each client.</p>
       </div>
@@ -16,10 +16,10 @@ function AboutUsDesc() {
 
       </div>
       <div>
-        <p className="text-center text-[18px] md:text-2xl  pt-20">How we work</p>
-        <p className=" px-20 pt-4 lg:px-48 md:text-[16px] text-[10px]">Our process begins with a thorough consultation to understand your vision, needs and budget, ensuring our design approach aligns perfectly with your expectations. We then move on to the planning and design phase, where our team creates detailed plans and innovative designs, including 3D visualizations and material selections, tailored to your preferences. Once the design proposal and budget estimate are ready, we present them for your approval, making any necessary adjustments based on your feedback to ensure everything is just right.</p>
-        <p className=" px-20 pt-4 lg:px-48 md:text-[16px] text-[10px]">With your approval, we proceed to the construction phase, where our skilled professionals manage all aspects of building, from laying foundations to applying the final finishes, upholding the highest standards of quality and craftsmanship. After the construction is complete, we focus on interior and exterior finishing, adding the final touches, including decor and landscaping, to bring your dream space to life with meticulous attention to detail.</p>
-        <p className=" px-20 pt-4 lg:px-48 md:text-[16px] text-[10px]">Finally, we conduct a thorough walkthrough with you to ensure every detail meets your satisfaction, making any last adjustments as needed. We then hand over the completed project along with all necessary documentation and provide ongoing support to address any post-construction needs or queries, ensuring your complete satisfaction.</p>
+        <p className="text-center text-[18px] md:text-3xl  pt-20">How we work</p>
+        <p className=" px-20 pt-4 lg:px-50 md:text-[24px] text-[10px]">Our process begins with a thorough consultation to understand your vision, needs and budget, ensuring our design approach aligns perfectly with your expectations. We then move on to the planning and design phase, where our team creates detailed plans and innovative designs, including 3D visualizations and material selections, tailored to your preferences. Once the design proposal and budget estimate are ready, we present them for your approval, making any necessary adjustments based on your feedback to ensure everything is just right.</p>
+        <p className=" px-20 pt-4 lg:px-50 md:text-[24px] text-[10px]">With your approval, we proceed to the construction phase, where our skilled professionals manage all aspects of building, from laying foundations to applying the final finishes, upholding the highest standards of quality and craftsmanship. After the construction is complete, we focus on interior and exterior finishing, adding the final touches, including decor and landscaping, to bring your dream space to life with meticulous attention to detail.</p>
+        <p className=" px-20 pt-4 lg:px-50 md:text-[24px] text-[10px]">Finally, we conduct a thorough walkthrough with you to ensure every detail meets your satisfaction, making any last adjustments as needed. We then hand over the completed project along with all necessary documentation and provide ongoing support to address any post-construction needs or queries, ensuring your complete satisfaction.</p>
       </div>
     </div>
   )
